a833e4a41ed8d7eb81fc9d3e8cb3f564381ddc83
klad <klad@DESKTOP-61NVODG>
2022-05-03 18:42:35 +0300

    Add project files.

---
+++ my_vector_2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "my_vector.h"
+
+int main()
+{
+	containers::Vector<int> v1;
+	containers::Vector<int> v2(v1);
+	v2 = v1;
+	containers::Vector<int> v3(10);
+	v3[5] = 42;
+	std::cout << v3[5] << '\n';
+}
---
+++ my_vector_2/my_vector.h
@@ -0,0 +1,60 @@
+#pragma once
+
+namespace containers {
+
+
+template<typename T> class Buffer {
+protected:
+	size_t size_ = 0, capacity_ = 0;
+	T* data_ = nullptr;
+
+	Buffer() = default;
+	Buffer(size_t count) : capacity_(count), data_(new T[capacity_]) {}
+	Buffer(const Buffer& other)
+		: size_(other.size_), capacity_(other.capacity_), data_(new T[capacity_]) {
+		std::copy(other.data_, other.data_ + capacity_, data_);
+	}
+	Buffer& operator=(const Buffer& other) {
+		if (this == &other) return *this;
+		delete[] data_;
+		size_ = other.capacity_;
+		capacity_ = other.capacity_;
+		data_ = new T[capacity_];
+		std::copy(other.data_, other.data_ + capacity_, data_);
+		return *this;
+	}
+	~Buffer() { delete[] data_; }
+};
+
+template<typename T> class Vector final : private Buffer<T> {
+	using Buffer<T>::size_;
+	using Buffer<T>::capacity_;
+	using Buffer<T>::data_;
+
+public:
+	Vector() = default;
+	Vector(const Vector& other) = default;
+	Vector& operator=(const Vector& other) = default;
+	Vector(size_t count) : Buffer<T>(count) {}
+	~Vector() = default;
+
+public: //element access
+	T& operator[](size_t pos) { return data_[pos]; }
+	const T& operator[](size_t pos) const { return data_[pos]; }
+
+	T& front() { return data_[0]; }
+	const T& front() const { return data_[0]; }
+
+	T& back() { return data_[size_ - 1]; }
+	const T& back() const { return data_[size_ - 1]; }
+
+	T* data() { return data_; }
+	const T* data() const { return data_; }
+
+public: //capacity
+	void assign() {
+
+	}
+};
+
+}
